717644623bdd9a24fbe0c00fc4b0180707351dec
saliha <salihasanu205@gmail.com>
2025-05-21 04:50:44 +0530

    Initial commit

---
+++ src/main/java/pl/edu/vistula/springframework/controller/hellocontroller.java
@@ -0,0 +1,25 @@
+package pl.edu.vistula.springframework.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+    @Controller
+   // @RestController
+    public class hellocontroller {
+
+        @GetMapping( "/")
+        public String hello() {
+            return "Hello Vistula, in my first Spring controller.";
+        }
+
+        @GetMapping("/greeting")
+        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+            model.addAttribute("name", name);
+            return "greeting";
+}
+    }
+
